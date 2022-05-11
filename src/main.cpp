01c15a1c331978f4affcbded1c40dace75d49592
terrill37 <walruswes@gmail.com>
2022-05-11 18:40:35 -0400

    backup

--- src/main.cpp
+++ src/main.cpp
@@ -16,40 +16,40 @@ int main(){
     return 0;
 }
 
-/*int main(int srgc, char* argv[]){
-    sf::Window window(sf::VideoMode(800,600),"SFML");
-    int frame=0;   
-    //std::cout<<"event type: "<< Event.type<<std::endl
-    std::cout<<"keypressed: "<<sf::Event::KeyPressed<<std::endl;
-    std::cout<<"MouseButtonReleased: "<<sf::Event::MouseButtonReleased<<std::endl;
-    std::cout<<"MouseMoved: "<<sf::Event::MouseMoved<<std::endl;
-    std::cout<<"Closed: "<<sf::Event::Closed<<std::endl;
-    std::cout<<"Resized: "<<sf::Event::Resized<<std::endl;
-    std::cout<<"GainedFocus: "<<sf::Event::GainedFocus<<std::endl;
-    std::cout<<"MouseEntered: "<<sf::Event::MouseEntered<<std::endl;
-    while(window.isOpen()){
-        frame++;
-        //std::cout<<"window open"<<std::endl;
-        int i=0;
-        int k=0;
-        sf::Event Event;
-        while(window.pollEvent(Event)){
-            //std::cout<<"event polled"<<std::endl;
-            std::cout<<"event type: "<< Event.type<<std::endl;
-            //std::cout<<"keypressed: "<<sf::Event::KeyPressed<<std::endl;
-            //std::cout<<"MouseButtonReleased: "<<sf::Event::MouseButtonReleased<<std::endl;
-            if(Event.type==sf::Event::Closed)
-                window.close();
-            if(Event.type==sf::Event::KeyPressed){
-                k++;
-                if(Event.key.code==sf::Keyboard::Escape)
-                    window.close();
-            }
-            i++;
-        }
-        window.display();
-        if(i!=0) std::cout<<frame<<" -> "<<i<<" "<<k<<std::endl;
-    }
-    return 0;
-}*/
+//int main(int srgc, char* argv[]){
+//    sf::Window window(sf::VideoMode(800,600),"SFML");
+//    int frame=0;   
+//    //std::cout<<"event type: "<< Event.type<<std::endl
+//    std::cout<<"keypressed: "<<sf::Event::KeyPressed<<std::endl;
+//    std::cout<<"MouseButtonReleased: "<<sf::Event::MouseButtonReleased<<std::endl;
+//    std::cout<<"MouseMoved: "<<sf::Event::MouseMoved<<std::endl;
+//    std::cout<<"Closed: "<<sf::Event::Closed<<std::endl;
+//    std::cout<<"Resized: "<<sf::Event::Resized<<std::endl;
+//    std::cout<<"GainedFocus: "<<sf::Event::GainedFocus<<std::endl;
+//    std::cout<<"MouseEntered: "<<sf::Event::MouseEntered<<std::endl;
+//    while(window.isOpen()){
+//        frame++;
+//        //std::cout<<"window open"<<std::endl;
+//        int i=0;
+//        int k=0;
+//        sf::Event Event;
+//        while(window.pollEvent(Event)){
+//            //std::cout<<"event polled"<<std::endl;
+//            std::cout<<"event type: "<< Event.type<<std::endl;
+//            //std::cout<<"keypressed: "<<sf::Event::KeyPressed<<std::endl;
+//            //std::cout<<"MouseButtonReleased: "<<sf::Event::MouseButtonReleased<<std::endl;
+//            if(Event.type==sf::Event::Closed)
+//                window.close();
+//            if(Event.type==sf::Event::KeyPressed){
+//                k++;
+//                if(Event.key.code==sf::Keyboard::Escape)
+//                    window.close();
+//            }
+//            i++;
+//        }
+//        window.display();
+//        if(i!=0) std::cout<<frame<<" -> "<<i<<" "<<k<<std::endl;
+//    }
+//    return 0;
+//}
     
